351ccd2754f883c9aa1e978f9f9ef639905427a6
Dino Chiesa <dchiesa@google.com>
2019-09-27 14:12:36 -0700

    update the provisioning tool to be more resilient
      conditionally create each entity
      also improve error handling
      pre-req a more current version of apigee-edge-js
      emit more shell variables to make it easier

--- tools/provision.js
+++ tools/provision.js
@@ -5,10 +5,10 @@
 // provision an Apigee Edge API Product, Developer, App, and Cache
 // for the OAuthV2 PKCE Example.
 //
-// Copyright 2017-2018 Google LLC.
+// Copyright 2017-2019 Google LLC.
 //
 
-/* jshint esversion: 6, strict:false */
+/* jshint esversion: 9, strict:implied */
 
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,38 +23,62 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2018-December-04 13:25:40>
+// last saved: <2019-September-27 10:49:09>
 
-const edgejs = require('apigee-edge-js'),
-    common = edgejs.utility,
-    apigeeEdge = edgejs.edge,
-    sprintf = require('sprintf-js').sprintf,
-    Getopt = require('node-getopt'),
-    version = '20181204-1324',
-    getopt = new Getopt(common.commonOptions.concat([
-      ['R' , 'reset', 'Optional. Reset, delete all the assets previously created by this script'],
-      ['e' , 'env=ARG', 'the Edge environment(s) to use for this demonstration. ']
-    ])).bindHelp();
+const edgejs     = require('apigee-edge-js'),
+      common     = edgejs.utility,
+      apigeeEdge = edgejs.edge,
+      util       = require('util'),
+      sprintf    = require('sprintf-js').sprintf,
+      Getopt     = require('node-getopt'),
+      version    = '20190927-0856',
+      getopt     = new Getopt(common.commonOptions.concat([
+        ['R' , 'reset', 'Optional. Reset, delete all the assets previously created by this script'],
+        ['e' , 'env=ARG', 'the Edge environment(s) to use for this demonstration. ']
+      ])).bindHelp();
 
 // ========================================================
+
+function random(min, max) {
+  let delta = max - min;
+  return Math.floor(Math.random() * delta) + min;
+}
+
+function pkceCodeVerifier() {
+  let s = '', desiredLength = random(43, 128);
+  while (s.length < desiredLength) {
+    s += Math.random().toString(36).substring(2, random(8, 12));
+  }
+  return s.substring(0, 128);
+}
+
+function sha256_base64url(s) {
+  return require('crypto')
+    .createHash('sha256')
+    .update(s)
+    .digest('base64')
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/\=+$/, '');
+}
 
 console.log(
   'Apigee Edge PKCE Example Provisioning tool, version: ' + version + '\n' +
     'Node.js ' + process.version + '\n');
 
 common.logWrite('start');
-var opt = getopt.parse(process.argv.slice(2));
+let opt = getopt.parse(process.argv.slice(2));
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
 const constants = {
-        cacheName      : 'cache1',
-        productName    : 'PKCE-Example-Product',
-        developerEmail : 'PKCE-Example-Developer@example.com',
-        appName        : 'PKCE-Example-App-1',
+        discriminators : {
+          cache        : 'cache1',
+          product      : 'PKCE-Example-Product',
+          developer    : 'PKCE-Example-Developer@example.com',
+          developerapp : 'PKCE-Example-App-1'
+        },
         note           : 'created '+ (new Date()).toISOString() + ' for PKCE Token-granting Example',
         callbackUrl    : 'https://dinochiesa.github.io/pkce-redirect/callback-handler.html',
         scopes         : ['A', 'B', 'C'],
-        appExpiry      : '180d'
+        appExpiry      : '210d'
       },
       connectOptions = {
         mgmtServer : opt.options.mgmtserver,
@@ -65,61 +89,96 @@ const constants = {
         verbosity  : opt.options.verbose || 0
       };
 
-
 apigeeEdge.connect(connectOptions)
-  .then( (org) => {
+  .then( org => {
     common.logWrite('connected');
     if (opt.options.reset) {
       let delOptions ={
-            app: { appName: constants.appName, developerEmail: constants.developerEmail },
-            developer:  { developerEmail: constants.developerEmail },
-            product : { productName: constants.productName }
+            app: { appName: constants.discriminators.developerapp, developerEmail: constants.discriminators.developer },
+            developer:  { developerEmail: constants.discriminators.developer },
+            product : { productName: constants.discriminators.product }
           };
 
       return org.developerapps.del(delOptions.app)
-        .then( (result) => org.developers.del(delOptions.developer) )
-        .then( (result) => org.products.del(delOptions.product) )
-        .then( (result) => common.logWrite(sprintf('ok. demo assets have been deleted')) )
-        .catch( (e) => console.log(e.stack) );
+        .then( _ => org.developers.del(delOptions.developer) )
+        .then( _ => org.products.del(delOptions.product) )
+        .then( _ => common.logWrite(sprintf('ok. demo assets have been deleted')) );
     }
 
-    let createOptions = {
-          cache: { cacheName : constants.cacheName, environment : opt.options.env },
-          product: {
-            productName  : constants.productName,
-            description  : 'Test Product for PKCE OauthV2 Example',
-            scopes       : constants.scopes,
-            attributes   : { access: 'public', note: constants.note },
-            approvalType : 'auto'
+    let options = {
+          caches: {
+            get: { environment : opt.options.env },
+            create: { cacheName : constants.discriminators.cache, environment : opt.options.env }
           },
-          developer: {
-            developerEmail : constants.developerEmail,
-            lastName       : 'Developer',
-            firstName      : 'PKCE-Example',
-            userName       : 'PKCE-Example-Developer',
-            attributes     : { note: constants.note }
+          products: {
+            create: {
+              productName  : constants.discriminators.product,
+              description  : 'Test Product for PKCE OauthV2 Example',
+              scopes       : constants.scopes,
+              attributes   : { access: 'public', note: constants.note },
+              approvalType : 'auto'
+            }
           },
-          app: {
-            developerEmail : constants.developerEmail,
-            appName        : constants.appName,
-            apiProduct     : constants.productName,
-            callbackUrl    : constants.callbackUrl,
-            expiry         : constants.appExpiry,
-            attributes     : { note: constants.note }
+          developers: {
+            create: {
+              developerEmail : constants.discriminators.developer,
+              lastName       : 'Developer',
+              firstName      : 'PKCE-Example',
+              userName       : 'PKCE-Example-Developer',
+              attributes     : { note: constants.note }
+            }
+          },
+          developerapps: {
+            get: {
+              developerEmail : constants.discriminators.developer
+            },
+            create: {
+              appName      : constants.discriminators.developerapp,
+              productName  : constants.discriminators.product,
+              description  : 'Test Product for PKCE OauthV2 Example',
+              scopes       : constants.scopes,
+              attributes   : { access: 'public', note: constants.note },
+              approvalType : 'auto'
+            }
           }
         };
 
-    return org.caches.create(createOptions.cache)
-      .then( (result) => org.products.create(createOptions.product) )
-      .then( (result) => org.developers.create(createOptions.developer) )
-      .then( (result) => org.developerapps.create(createOptions.app) )
-      .then( (result) => {
-        common.logWrite(sprintf('created app. name: %s', result.name));
+    function conditionallyCreateEntity(entityType) {
+      let collectionName = entityType + 's';
+      return org[collectionName].get(options[collectionName].get || {})
+        .then( result => {
+          //console.log('GET Result: ' + JSON.stringify(result));
+          if (result.indexOf(constants.discriminators[entityType])>=0) {
+            if (collectionName == 'developerapps') {
+              return org[collectionName].get({
+                developerEmail : constants.discriminators.developer,
+                appName        : constants.discriminators.developerapp
+              });
+            }
+            return Promise.resolve(result) ;
+          }
+          return org[collectionName].create(options[collectionName].create);
+        });
+    }
+
+    return conditionallyCreateEntity('cache')
+      .then( _ => conditionallyCreateEntity('product'))
+      .then( _ => conditionallyCreateEntity('developer'))
+      .then( _ => conditionallyCreateEntity('developerapp'))
+      .then( result => {
+        common.logWrite(sprintf('app name: %s', result.name));
         console.log(sprintf('\n\nORG=%s', opt.options.org));
         console.log(sprintf('ENV=%s', opt.options.env));
         console.log(sprintf('client_id=%s', result.credentials[0].consumerKey));
         console.log(sprintf('client_secret=%s', result.credentials[0].consumerSecret));
-      })
-      .catch( (e) => console.log(e.stack) );
+        console.log(sprintf('redirect_uri=%s', constants.callbackUrl));
+        let codeVerifier = pkceCodeVerifier();
+        console.log(sprintf('code_verifier=%s', codeVerifier));
+        console.log(sprintf('code_challenge=%s', sha256_base64url(codeVerifier)));
+        console.log();
+
+        console.log('curl -i -X GET "https://$ORG-$ENV.apigee.net/20181127/oauth2-ac-pkce/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=A&code_challenge=${code_challenge}&code_challenge_method=S256"');
+
+      });
   })
-  .catch( (e) => console.log(e.stack) );
+  .catch( e => console.log(util.format(e)) );
