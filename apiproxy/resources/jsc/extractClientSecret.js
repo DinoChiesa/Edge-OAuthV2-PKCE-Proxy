5f831e200b4e8b05163244c8456cb0590d30d153
Dino Chiesa <dchiesa@google.com>
2020-08-19 15:45:46 -0700

    update extractConsumerSecret to handle normal case

--- apiproxy/resources/jsc/extractClientSecret.js
+++ apiproxy/resources/jsc/extractClientSecret.js
@@ -3,7 +3,6 @@
 // Determine the consumer_secret for the given client_id.
 // This policy scans variables set by a preceding AccessEntity policy.
 
-
 /* jshint esversion:6, node:false, strict:implied */
 /* global context, properties */
 var policyName = properties['access-entity-policy-name'];
@@ -11,11 +10,23 @@ var varNamePrefix = 'AccessEntity.ChildNodes.'+ policyName +'.App.Credentials.Cr
 var clientId = context.getVariable('request.formparam.client_id');
 var consumerSecret = null;
 
-// scan the first 9 credentials
-for (var i = 1; i<10 && !consumerSecret; i++) {
-  var consumerKey = context.getVariable(varNamePrefix + i + '.ConsumerKey');
-  if (clientId == consumerKey) {
-    consumerSecret = context.getVariable(varNamePrefix + i + '.ConsumerSecret');
+// If there is one credential, then the variable is varNamePrefix + '.ConsumerKey'
+// if there are multiple credentials, then the variable is varNamePrefix + '.INDEX.ConsumerKey'
+// where INDEX is replaced with [1,2,3,4....]
+
+// first look for the "one credentia" case.
+var consumerKey = context.getVariable(varNamePrefix + 'ConsumerKey');
+if (clientId == consumerKey) {
+  consumerSecret = context.getVariable(varNamePrefix + 'ConsumerSecret');
+}
+
+if ( ! consumerSecret) {
+  // if not that, then scan the first 9 credentials.
+  for (var i = 1; i<10 && !consumerSecret; i++) {
+    consumerKey = context.getVariable(varNamePrefix + i + '.ConsumerKey');
+    if (clientId == consumerKey) {
+      consumerSecret = context.getVariable(varNamePrefix + i + '.ConsumerSecret');
+    }
   }
 }
 
